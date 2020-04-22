0f2a79dedb436e126945484b7e1b8ce14bc9566d
Monkey <yzjacc@gmail.com>
2020-04-22 09:39:11 +0800

    Initial commit

---
+++ src/components/GlobalFooter/GlobalFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="footer">
+    <div class="copyright">
+      Copyright
+      <a-icon type="copyright" /> 2020 <span>哈尔滨理工大学出品</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalFooter',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.footer {
+  padding: 0 16px;
+  margin: 48px 0 24px;
+  text-align: center;
+
+  .copyright {
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 14px;
+  }
+}
+</style>
---
+++ src/config/router.config.js
@@ -0,0 +1,176 @@
+// eslint-disable-next-line
+import { UserLayout, BasicLayout, RouteView, BlankLayout, PageView } from '@/layouts'
+import { bxAnaalyse } from '@/core/icons'
+
+export const asyncRouterMap = [
+
+  {
+    path: '/',
+    name: 'index',
+    component: BasicLayout,
+    meta: { title: '首页' },
+    redirect: '/dashboard/pro-basic',
+    children: [
+      // dashboard
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        redirect: '/dashboard/pro-basic',
+        component: RouteView,
+        meta: { title: '项目管理', keepAlive: true, icon: bxAnaalyse, permission: [ 'dashboard' ] },
+        children: [
+          {
+            path: '/dashboard/pro-basic',
+            name: 'Basic',
+            component: () => import('@/views/dashboard/Analysis'),
+            meta: { title: '项目基本信息', keepAlive: true, permission: [ 'dashboard' ] }
+          },
+          {
+            path: '/dashboard/pro-mess',
+            name: 'Message',
+            component: () => import('@/views/dashboard/Analysis'),
+            meta: { title: '项目信息', keepAlive: false, permission: [ 'dashboard' ] }
+          },
+          {
+            path: '/dashboard/pro-person',
+            name: 'Person',
+            component: () => import('@/views/dashboard/Workplace'),
+            meta: { title: '项目人员', keepAlive: true, permission: [ 'dashboard' ] }
+          }
+        ]
+      },
+
+      // forms
+      {
+        path: '/form',
+        redirect: '/form/base-form',
+        component: PageView,
+        meta: { title: '劳务信息', icon: 'form', permission: [ 'form' ] },
+        children: [
+          {
+            path: '/form/worker-mess',
+            name: 'WorkerMessage',
+            component: () => import('@/views/form/stepForm/StepForm'),
+            meta: { title: '劳工信息', keepAlive: true, permission: [ 'form' ] }
+          },
+          {
+            path: '/form/worker-manage',
+            name: 'Manage',
+            component: () => import('@/views/form/stepForm/StepForm'),
+            meta: { title: '工种管理', keepAlive: true, permission: [ 'form' ] }
+          }
+        ]
+      },
+
+      // list
+      {
+        path: '/list',
+        name: 'list',
+        component: PageView,
+        redirect: '/list/table-list',
+        meta: { title: '考勤管理', icon: 'table', permission: [ 'table' ] },
+        children: [
+          {
+            path: '/list/check-worker/:pageNo([1-9]\\d*)?',
+            name: 'CheckWorker',
+            hideChildrenInMenu: true, // 强制显示 MenuItem 而不是 SubMenu
+            component: () => import('@/views/list/TableList'),
+            meta: { title: '考勤机管理', keepAlive: true, permission: [ 'table' ] }
+          },
+          {
+            path: '/list/clock',
+            name: 'Clock',
+            component: () => import('@/views/list/StandardList'),
+            meta: { title: '打卡记录', keepAlive: true, permission: [ 'table' ] }
+          },
+          {
+            path: '/list/card',
+            name: 'Card',
+            component: () => import('@/views/list/CardList'),
+            meta: { title: '补卡登记', keepAlive: true, permission: [ 'table' ] }
+          },
+          {
+            path: '/list/table',
+            name: 'Table',
+            component: () => import('@/views/list/search/SearchLayout'),
+            redirect: '/list/search/article',
+            meta: { title: '考勤报表', keepAlive: true, permission: [ 'table' ] }
+          }
+        ]
+      },
+      // Exception
+      {
+        path: '/exception',
+        name: 'exception',
+        component: RouteView,
+        redirect: '/exception/test',
+        meta: { title: '疫情测温', icon: 'warning', permission: [ 'exception' ] },
+        children: [
+          {
+            path: '/exception/test',
+            name: 'Test',
+            component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/403'),
+            meta: { title: '疫情报表', permission: [ 'exception' ] }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '*', redirect: '/404', hidden: true
+  }
+]
+
+/**
+ * 基础路由
+ * @type { *[] }
+ */
+export const constantRouterMap = [
+  {
+    path: '/user',
+    component: UserLayout,
+    redirect: '/user/login',
+    hidden: true,
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Login')
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Register')
+      },
+      {
+        path: 'register-result',
+        name: 'registerResult',
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/RegisterResult')
+      },
+      {
+        path: 'recover',
+        name: 'recover',
+        component: undefined
+      }
+    ]
+  },
+
+  {
+    path: '/test',
+    component: BlankLayout,
+    redirect: '/test/home',
+    children: [
+      {
+        path: 'home',
+        name: 'TestHome',
+        component: () => import('@/views/Home')
+      }
+    ]
+  },
+
+  {
+    path: '/404',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404')
+  }
+
+]
