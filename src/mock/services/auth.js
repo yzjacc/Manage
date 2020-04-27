0c5b4daeaf2b4096f8f2380043acf9d555f3308a
Monkey <yzjacc@gmail.com>
2020-04-27 11:52:20 +0800

    update readme

--- src/mock/services/auth.js
+++ src/mock/services/auth.js
@@ -1,7 +1,7 @@
 import Mock from 'mockjs2'
 import { builder, getBody } from '../util'
 
-const username = ['admin', 'super']
+const username = ['admin']
 // 强硬要求 ant.design 相同密码
 // '21232f297a57a5a743894a0e4a801fc3',
 const password = ['8914de686ab28dc22f30d3d8e107ff6c'] // admin, ant.design
